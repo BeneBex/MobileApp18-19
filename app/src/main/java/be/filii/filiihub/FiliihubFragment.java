55b57734d275afa690821a7f959c4d2a4c7af17a
Unknown <Benedicte.bex@student.pxl.be>
2018-10-14 13:17:47 +0200

    Bottomnavigation and fragmentviews

---
+++ app/src/main/java/be/filii/filiihub/FiliihubFragment.java
@@ -0,0 +1,21 @@
+package be.filii.filiihub;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FiliihubFragment extends Fragment {
+    public static Fragment newInstance() {
+        return new FiliihubFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_filiihub, container, false);
+    }
+}
--- app/src/main/java/be/filii/filiihub/MainActivity.java
+++ app/src/main/java/be/filii/filiihub/MainActivity.java
@@ -1,13 +1,47 @@
 package be.filii.filiihub;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment = null;
+                switch(menuItem.getItemId()){
+                    case R.id.navigation_filiihub:
+                        selectedFragment = FiliihubFragment.newInstance();
+                    break;
+                    case R.id.navigation_event:
+                        selectedFragment = EventFragment.newInstance();
+                        break;
+
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frameLayout, selectedFragment);
+                transaction.commit();
+                return true;
+            }
+        });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, FiliihubFragment.newInstance());
+        transaction.commit();
     }
+
 }
